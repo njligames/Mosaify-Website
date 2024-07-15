3486a612b35dfdfe08c57fab4f469e5374286137
James Folk <jamesfolk1@gmail.com>
2024-07-15 16:43:27 -0400

    Now only using one thread

--- docker.sh
+++ docker.sh
@@ -5,6 +5,6 @@ docker build -t mosaify-website .
 
 # Run the Docker container
 # docker run --cpu-quota 50000 --memory 8g --env-file .env -p 8000:8000 mosaify-website
-docker run --env-file .env -p 8000:8000 mosaify-website
+docker run -m 4g --memory-swap 4g --env-file .env_docker -p 8000:8000 mosaify-website
 
 
